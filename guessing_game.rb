ff5dd8e1370ddf255c48a8c96e1c2e183d90ab93
Matt Rice <mattrice12@outlook.com>
2016-06-21 14:57:24 -0500

    Game works now. Need to refactor

--- guessing_game.rb
+++ guessing_game.rb
@@ -23,7 +23,6 @@
 
 # Classes = Number_set, Number_generator, Player, Game
 
-
 #Data: Numbers 1-100
 #Behavior: Holds set
 class Number_gen
@@ -50,15 +49,75 @@ end
 
 class Player
   attr_accessor :name
-  def initialize
+  def initialize(name)
     @name = name
   end
 end
 
 class Game
-  attr_reader :name
+  attr_reader :name, :game_number
   def initialize
-    @player   = Players.new(@name)
-    @computer = Players.new("Skynet")
+    @name = welcome
+    @player   = Player.new(@name)
+    @computer = Player.new("Skynet")
+    @game_number = Number_gen.new
+  end
+
+  def welcome
+    system("clear")
+    puts "Welcome to the Guessing Game!"
+    puts
+    print "Please input your name. > "
+    gets.chomp
+  end
+
+  def greeting
+    system("clear")
+    puts "Welcome to the Guessing Game!"
+    puts
+    puts "Hello, #{@player.name}! Today you will be playing #{@computer.name}!"
+    puts
+  end
+
+  def explain_rules
+    puts "The rules of the game are simple! Guess a number between 1 and 100, and I will tell you if you are too high or too low. 5 wrong guesses, and you lose!"
+    puts
+  end
+
+  def guess_number
+    puts "Please guess a number from 1 to 100. > "
+    gets.chomp.to_i
+  end
+
+  def start
+    greeting
+    explain_rules
+    ran_number = @game_number.num_gen
+    count = 0
+    loop do
+      response = guess_number
+      if response == ran_number
+        puts "You got the number! You win!"
+        break
+      elsif response > ran_number
+        puts
+        puts "Too high!"
+        count += 1
+      elsif response < ran_number
+        puts
+        puts "Too low!"
+        count += 1
+      else
+        puts
+        puts "Incorrect input, please guess again!"
+      end
+      if count >= 5
+        puts
+        puts "I've given you 5 chances to guess the number #{ran_number}, but you couldn't guess it. YOU LOSE!"
+        break
+      end
+    end
   end
 end
+
+Game.new.start
